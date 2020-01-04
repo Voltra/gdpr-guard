435083734578cab54102cc2171d6661429fb6b5e
Voltra <ludwig.guerin.98@gmx.fr>
2020-01-04 14:08:27 +0100

    Add doc for GdprManagerBuilder

--- src/builders/GdprManagerBuilder.ts
+++ src/builders/GdprManagerBuilder.ts
@@ -3,34 +3,88 @@ import { GdprStorage } from "../GdprStorage"
 import { GdprManager } from "../GdprManager"
 import { GdprGroupBuilder } from "./builders"
 
+/**
+ * Builder for a GdprManager
+ * @class GdprManagerBuilder
+ * @export
+ */
 class GdprManagerBuilder{
     public storage: GdprStorage = GdprStorage.Cookie;
     public groups: GdprGuardGroup[] = [];
 
-    public static make(){
+    /**
+     * Factory for a builder
+     * @static
+     * @returns {GdprManagerBuilder}
+     * @memberof GdprManagerBuilder
+     */
+    public static make(): GdprManagerBuilder{
         return new GdprManagerBuilder();
     }
 
+    /**
+     * Start a new group
+     * @param {?GdprStorage} [storage=null] The storage type of the group
+     * @param {string} [name=""] The name of the group
+     * @param {string} [description=""] The description of the group
+     * @param {boolean} [enabled=true] Whether or not the group is enabled
+     * @returns {GdprGroupBuilder}
+     * @memberof GdprManagerBuilder
+     */
     startGroup(storage: GdprStorage|null = null, name: string = "", description: string = "", enabled: boolean = true): GdprGroupBuilder{
         return GdprGroupBuilder.create(this, name, description, storage, enabled);
     }
 
+    /**
+     * Start a new group as required
+     * @param {?GdprStorage} [storage=null] The storage type of the group
+     * @param {string} [name=""] The name of the group
+     * @param {string} [description=""] The description of the group
+     * @returns {GdprGroupBuilder}
+     * @memberof GdprManagerBuilder
+     */
     startRequiredGroup(storage: GdprStorage|null = null, name: string = "", description: string = ""){
         return this.startGroup(storage, name, description, true).enabled();
     }
 
+    /**
+     * Start a new enabled group
+     * @param {?GdprStorage} [storage=null] The storage type of the group
+     * @param {string} [name=""] The name of the group
+     * @param {string} [description=""] The description of the group
+     * @returns {GdprGroupBuilder}
+     * @memberof GdprManagerBuilder
+     */
     startEnabledGroup(storage: GdprStorage|null = null, name: string = "", description: string = ""): GdprGroupBuilder{
         return this.startGroup(storage, name, description, true);
     }
 
+    /**
+     * Start a new disabled group
+     * @param {?GdprStorage} [storage=null] The storage type of the group
+     * @param {string} [name=""] The name of the group
+     * @param {string} [description=""] The description of the group
+     * @returns {GdprGroupBuilder}
+     * @memberof GdprManagerBuilder
+     */
     startDisabledGroup(storage: GdprStorage|null = null, name: string = "", description: string = ""): GdprGroupBuilder{
         return this.startGroup(storage, name, description, false);
     }
 
+    /**
+     * Build the manager from the current builder state
+     * @returns {GdprManager}
+     * @memberof GdprManagerBuilder
+     */
     build(): GdprManager{
         return GdprManager.create(this.groups);
     }
 
+    /**
+     * End this group's creation (no-op for manager builders)
+     * @returns {GdprManagerBuilder}
+     * @memberof GdprManagerBuilder
+     */
     endGroup(): GdprManagerBuilder{
         return this;
     }
