18f75fb9f13418b968babd3bfe915cd3d0a39a98
Voltra <ludwig.guerin.98@gmx.fr>
2020-06-19 15:51:50 +0200

    Add savior adapter

--- src/serde/GdprSavior.ts
+++ src/serde/GdprSavior.ts
@@ -5,7 +5,7 @@ import { GdprManager } from "../GdprManager";
  * @typedef GdprManagerFactory
  * @export
  */
-type GdprManagerFactory = () => GdprManager;
+type GdprManagerFactory = () => Promise<GdprManager>;
 
 /**
  * Handle saving/restoring/checking semantics
@@ -15,13 +15,13 @@ type GdprManagerFactory = () => GdprManager;
 interface GdprSavior{
 	/**
 	 * Restore the manager (saved state)
-	 * @param shouldUpdate - Whether or not it should update its savior internals
+	 * @param shouldUpdate - Whether or not it should update its savior internals (should default to true)
 	 */
 	restore(shouldUpdate?: boolean): Promise<GdprManager|null>;
 
 	/**
 	 * Determine whether or not there is already an existing manager (saved state)
-	 * @param shouldUpdate - Whether or not it should update its savior internals
+	 * @param shouldUpdate - Whether or not it should update its savior internals (should default to true)
 	 */
 	exists(shouldUpdate?: boolean): Promise<boolean>;
 
@@ -43,9 +43,56 @@ interface GdprSavior{
 	 * @param manager - The manager to store (state to save)
 	 */
 	storeIfNotExists(manager: GdprManager): Promise<boolean>;
+
+	/**
+	 * Handle shared state updates
+	 * @param manager - The new manager to use
+	 */
+	updateSharedManager(manager: GdprManager): Promise<void>;
+
+	/**
+	 * Check if there is an existing manager state (should rely on GdprSavior#exists)
+	 */
+	check(): Promise<void>;
+}
+
+abstract class GdprSaviorAdapter implements GdprSavior{
+	public abstract restore(shouldUpdate?: boolean): Promise<GdprManager|null>;
+	public abstract store(manager: GdprManager): Promise<boolean>;
+	public abstract updateSharedManager(manager: GdprManager): Promise<void>;
+
+	public async exists(shouldUpdate: boolean = true): Promise<boolean>{
+		const restored = await this.restore(shouldUpdate);
+		return restored !== null;
+	}
+
+	public async storeIfNotExists(manager: GdprManager): Promise<boolean>{
+		const exists = await this.exists();
+		return exists ? true : this.store(manager);
+	}
+
+	public async restoreOrCreate(factory: GdprManagerFactory): Promise<GdprManager>{
+		const restored = await this.restore();
+
+		if(!restored){
+			const generated = await factory();
+			this.updateSharedManager(generated);
+			return generated;
+		}
+
+		return restored;
+	}
+
+	public async check(): Promise<void>{
+		const _ = await Promise.resolve();
+		setTimeout(() => {
+			this.exists(true);
+		}, 100);
+	}
 }
 
 export {
 	GdprManagerFactory,
 	GdprSavior,
+	GdprSaviorAdapter,
 }
