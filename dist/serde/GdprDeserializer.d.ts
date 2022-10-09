c8b85067ddea78682e305e07f80ee2cd51977e8a
Voltra <ludwig.guerin.98@gmx.fr>
2022-10-09 22:24:21 +0200

    -> v2.2.6

--- dist/serde/GdprDeserializer.d.ts
+++ dist/serde/GdprDeserializer.d.ts
@@ -1,8 +1,9 @@
 import { GdprManager } from "../GdprManager";
 import { GdprGuardGroup } from "../GdprGuardGroup";
 import { GdprGuard } from "../GdprGuard";
+import { GdprManagerRaw } from "../../dist/GdprManager";
 declare abstract class GdprDeserializer {
-    static manager(raw: any): GdprManager | null;
+    static manager(raw: GdprManagerRaw | any): GdprManager | null;
     static group(raw: any): GdprGuardGroup | null;
     static guard(raw: any): GdprGuard | null;
 }
